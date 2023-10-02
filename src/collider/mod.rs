70704469d0f4f129380afb636de6fcfc1debd260
tom <tom@live.se>
2023-10-02 13:20:48 +0200

    pathing start

--- src/collider/mod.rs
+++ src/collider/mod.rs
@@ -14,12 +14,10 @@ struct BoxCollider {
 }
 
 struct BroadTree {
-    array: Box<[QuadTree; 4]>,
+    array: Box<Option<[BroadTree; 4]>>,
     bound: (u32, u32),
 }
 
-impl QuadTree {
-    fn push_entity(entity: Entity) {
-
-    }
+impl BroadTree {br
+    fn push_entity(entity: Entity) {}
 }
